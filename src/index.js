97e9731ba6438443a6d0afb0032344e020abefec
Marta Torres Burguillo <99444191+burguimarta@users.noreply.github.com>
2022-05-17 11:36:13 +0200

    exercise 2: get the movie done

--- src/index.js
+++ src/index.js
@@ -13,36 +13,45 @@ const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
+// Array guardado en movies para poder usarlo fuera
+const movies = [
+  {
+    id: '1',
+    title: 'Gambita de dama',
+    gender: 'Drama',
+    image: '//localhost:4000/gambita-de-dama.jpg'
+  },
+  {
+    id: '2',
+    title: 'Friends',
+    gender: 'Comedia',
+    image: '//localhost:4000/friends.jpg'
+  }
+];
 
 server.get('/movies', (req, res) => {
   const response = {
-
     success: true,
-    movies: [
-      {
-        id: '1',
-        title: 'Gambita de dama',
-        gender: 'Drama',
-        image: '//localhost:4000/gambita-de-dama.jpg'
-      },
-      {
-        id: '2',
-        title: 'Friends',
-        gender: 'Comedia',
-        image: '//localhost:4000/friends.jpg'
-      }
-
-    ]
-
+    // Sacado el array en una constante y aquí se devuelve con el nombre de la constante
+    movies: movies
   };
   res.json(response)
 });
-
+// Para conseguir el id de la película que se va a renderizar con movieId
 server.get('/movie/:movieId', (req, res) => {
+  // dentro del server.get hay que crear una constante para encontrar el id de las películas con req.params.movieId
+  const foundMovie = movies.find(movie => movie.id === req.params.movieId);
+  // Este es el primer console.log que en el ejercicio 1 de la lección 4
   console.log(req.params.movieId);
+  // Los dos console.log muestran exactamente lo mismo
+  console.log(foundMovie);
+  // Esta respuesta la he puesto para que el navegador no se quede pillado sin encontrar una respuesta que devolver
+  res.json(null);
 });
 
-const foundMovie = movieId.find();
+
+
+
 
 
 
